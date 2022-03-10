3b6e5f7ac91459a1dfb0ff72acc8b29c1b5e222e
Faith_Watiri <watiri216@gmail.com>
2022-03-10 05:08:15 -0800

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,1 @@
+#include<stdio.h> /** * main - Prints string *Description: Using printf * Return:0 */ int main(void) {printf("%S", "with proper grammar, but the outcome is a piece of art,\n"};return (0);}

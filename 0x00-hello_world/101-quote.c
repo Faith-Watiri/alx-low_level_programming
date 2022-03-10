f74a090f1cede6b4de6726d595e4ac5bb288cff8
Faith_Watiri <watiri216@gmail.com>
2022-03-10 12:22:19 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
+#include <unstid.h>
 /**
- * main - outputs to string
- *
- * Description: Prints out "and that piece of art is useful..." without printf
- *
- * Return: 1
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
- char *S= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
- long l = 59;
- long fd = 1;
- long sycall = 1;
- long ret = 0;
- _asm_("sycall"
- : "=a" (ret)
- : "a" (sycall)'
- "D" (fd),
- "S" (s),
- "d" (l));
+ char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ write(2, ch, sizeof(ch));
  return (1);
 } 

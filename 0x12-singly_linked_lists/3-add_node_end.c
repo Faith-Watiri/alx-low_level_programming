19d304aa0b7b7c504ce592244eb1a2fd4e13e0b6
Faith-Watiri <watiri216@gmail.com>
2024-09-13 09:55:32 -0700

    commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,45 +2,52 @@
 #include <string.h>
 #include "lists.h"
 
-/**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: Double pointer to the head of the list.
- * @str: String to be added as the new node's str (duplicated).
- *
- * Return: Address of the new element, or NULL if it failed.
- */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	unsigned int len = 0;
+    list_t *new_node, *temp;
+    unsigned int len = 0;
 
-	if (str == NULL)
-		return (NULL);
+    if (str == NULL)
+        return (NULL);
 
-	/* Calculate the length of the string */
-	while (str[len])
-		len++;
+    /* Calculate the length of the string */
+    while (str[len])
+        len++;
 
-	/* Allocate memory for the new node */
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+    /* Allocate memory for the new node */
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	/* Duplicate the string */
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    /* Duplicate the string */
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
 
-	/* Set the length and next pointer */
-	new_node->len = len;
-	new_node->next = *head;
+    /* Set the length and next pointer */
+    new_node->len = len;
+    new_node->next = NULL;
 
-	/* Update the head pointer */
-	*head = new_node;
+    /* If the list is empty, set the new node as the head */
+    if (*head == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        /* Traverse to the end of the list */
+        temp = *head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        /* Attach the new node at the end */
+        temp->next = new_node;
+    }
 
-	return (new_node);
+    return (new_node);
 }
 

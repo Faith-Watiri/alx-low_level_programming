b39e7d87750c4bf85bfa690f3e4201e0fd36c4a3
Faith-Watiri <watiri216@gmail.com>
2024-09-13 10:07:02 -0700

    commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,20 @@
 section .data
-    message db 'Hello, Holberton', 0    ; The string to print, terminated by a null byte
+    message db 'Hello, Holberton', 0xA, 0  ; The string to print, including newline
 
 section .text
-    global main                         ; Entry point for the program
-
-extern printf                           ; Extern declaration of printf
+    global main                            ; Entry point for the program
+    extern printf                          ; Extern declaration of printf
 
 main:
     ; Set up the arguments for printf
-    mov rdi, message                    ; First argument (format string) is the address of the message
-    xor rax, rax                        ; Clear the rax register to indicate 0 floating point arguments
+    mov rdi, message                       ; First argument (format string) is the address of the message
+    xor rax, rax                           ; Clear rax (no floating-point arguments)
 
     ; Call printf
-    call printf                         ; Call the printf function
+    call printf                            ; Call the printf function
 
     ; Exit the program
-    mov rax, 60                         ; Syscall number for exit (60)
-    xor rdi, rdi                        ; Status code 0
-    syscall                             ; Make the syscall to exit
+    mov rax, 60                            ; Syscall number for exit (60)
+    xor rdi, rdi                           ; Status code 0
+    syscall                                ; Make the syscall to exit
 
